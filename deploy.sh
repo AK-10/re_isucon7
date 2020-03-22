a028538c7fa2021eeed1e7a0a7de481baf6d28ea
AK-10 <atsushi524k10@gmail.com>
2020-03-22 05:17:16 +0000

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# check TARGET_BRANCH existance
+if [ "$TARGET_BRANCH" = "" ]; then
+	echo "you must set TARGET_BRANCH"
+	exit 0
+fi
+
+# checkout branch
+echo "-------- fetch source ---------"
+HERE=/home/isucon/isubata
+git fetch origin "$TARGET_BRANCH":"$TARGET_BRANCH"
+git co "$TARGET_BRANCH"
+
+
+# compile
+echo "-------- compile source ---------"
+cd webapp/go
+make
+
+
+# restart services
+echo "-------- restart services ---------"
+sudo systemctl restart nginx.service
+sudo systemctl restart isubata.golang
+
+
+# run benchmark
+echo "-------- run benchmark ---------"
+cd "$HERE"/bench
+./bin/bench -remotes=127.0.0.1 -output result.json; cat result.json | jq
+
+# back project root
+cd "$HERE"
+
