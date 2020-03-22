72153f5c797a0b3444a99d5d22bf85ba607dfaf1
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-03-22 17:36:20 +0900

    fix queryMsgWithUsrs

--- webapp/go/src/isubata/message.go
+++ webapp/go/src/isubata/message.go
@@ -57,7 +57,7 @@ func queryMsgWithUsrs(chanID, lastID int64) ([]MsgWithUsr, error) {
 	msgWithUsrs := []MsgWithUsr{}
 
 	query := `SELECT u.name, u.display_name, u.avatar_icon, m.id, m.created_at, m.content FROM user u INNER JOIN message m ON u.id = m.user_id WHERE m.channel_id = ? AND m.id > ? ORDER BY m.id DESC LIMIT 100`
-	rows, err := db.Query(query, lastID, chanID)
+	rows, err := db.Query(query, chanID, lastID)
 
 	if err != nil {
 		return nil, err
