19fc6f1511fb8e9f1ed886690fa2286a70519456
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-03-29 03:40:12 +0900

    remove get value from env

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -362,13 +362,21 @@ class App < Sinatra::Base
     return @db_client if defined?(@db_client)
 
     @db_client = Mysql2::Client.new(
-      host: ENV.fetch('ISUBATA_DB_HOST') { 'localhost' },
-      port: ENV.fetch('ISUBATA_DB_PORT') { '3306' },
-      username: ENV.fetch('ISUBATA_DB_USER') { 'isucon' },
-      password: ENV.fetch('ISUBATA_DB_PASSWORD') { '' },
+      host: 'localhost',
+      port: '3306',
+      username: 'isucon',
+      password: '',
       database: 'isubata',
       encoding: 'utf8mb4'
     )
+    # @db_client = Mysql2::Client.new(
+    #   host: ENV.fetch('ISUBATA_DB_HOST') { 'localhost' },
+    #   port: ENV.fetch('ISUBATA_DB_PORT') { '3306' },
+    #   username: ENV.fetch('ISUBATA_DB_USER') { 'isucon' },
+    #   password: ENV.fetch('ISUBATA_DB_PASSWORD') { '' },
+    #   database: 'isubata',
+    #   encoding: 'utf8mb4'
+    # )
     @db_client.query('SET SESSION sql_mode=\'TRADITIONAL,NO_AUTO_VALUE_ON_ZERO,ONLY_FULL_GROUP_BY\'')
     @db_client
   end
