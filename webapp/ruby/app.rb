60b1ea96c3afa120f7dd7b27ab6d0dc0a1586b18
Atsushi KONISHI <atsushi524k10@gmail.com>
2020-03-29 03:48:06 +0900

    revert

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -154,8 +154,8 @@ class App < Sinatra::Base
     end
     response.reverse!
 
-    # max_message_id = rows.empty? ? 0 : rows.map { |row| row['m_id'] }.max
-    max_message_id = rows.empty? ? 0 : rows.first['m_id']
+    max_message_id = rows.empty? ? 0 : rows.map { |row| row['m_id'] }.max
+    # max_message_id = rows.empty? ? 0 : rows.first['m_id']
     statement = db.prepare([
       'INSERT INTO haveread (user_id, channel_id, message_id, updated_at, created_at) ',
       'VALUES (?, ?, ?, NOW(), NOW()) ',
